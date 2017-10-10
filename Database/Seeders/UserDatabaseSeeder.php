a3a3c1b454dfea98b21fe4e46bf2313b74d6a327
daniels.grietins <daniels.grietins@gmail.com>
2017-10-10 13:13:09 +0300

    Added seed for Test admin

--- Database/Seeders/UserDatabaseSeeder.php
+++ Database/Seeders/UserDatabaseSeeder.php
@@ -4,9 +4,11 @@ namespace Modules\User\Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use Nwidart\Modules\Facades\Module;
 
 class UserDatabaseSeeder extends Seeder
 {
+
     /**
      * Run the database seeds.
      *
@@ -17,5 +19,24 @@ class UserDatabaseSeeder extends Seeder
         Model::unguard();
 
         $this->call(MenuTableSeeder::class);
+
+        $userModel = config('auth.providers.users.model');
+        $user = $userModel::firstOrCreate(
+            [
+                'email' => 'admin@admin.com'
+            ],
+            [
+                'email'      => 'admin@admin.com',
+                'first_name' => 'Test Admin',
+                'password'   => bcrypt('admin'),
+                'is_admin'   => 1,
+            ]);
+
+        $module = Module::find('Permission');
+
+        if ($module && $module->enabled()) {
+            $user->role_id = 1;
+            $user->save();
+        }
     }
 }
