c4ed58f60da67446bbb684a0d97e56c9a90932ad
Dmitrijs Mihailovs <dimka62@inbox.lv>
2017-11-16 18:18:54 +0200

    Menu seeder update

--- Database/Seeders/MenuTableSeeder.php
+++ Database/Seeders/MenuTableSeeder.php
@@ -5,6 +5,7 @@ namespace Modules\User\Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 use Modules\Admin\Models\Menu;
+use Netcore\Translator\Helpers\TransHelper;
 
 class MenuTableSeeder extends Seeder
 {
@@ -20,26 +21,35 @@ class MenuTableSeeder extends Seeder
         $menus = [
             'leftAdminMenu' => [
                 [
-                    'name'   => 'Users',
-                    'icon'   => 'ion-ios-people',
-                    'type'   => 'route',
-                    'value'  => 'user::users.index',
+                    'name'            => 'Users',
+                    'icon'            => 'ion-ios-people',
+                    'type'            => 'route',
+                    'is_active'       => 1,
+                    'value'           => 'user::users.index',
                     'active_resolver' => 'user::users.*',
-                    'module' => 'User',
-                    'parameters' => json_encode([])
+                    'module'          => 'User',
+                    'parameters'      => json_encode([])
                 ]
             ]
         ];
 
-        foreach( $menus as $name => $items ) {
+        foreach ($menus as $name => $items) {
             $menu = Menu::firstOrCreate([
                 'name' => $name
             ]);
 
-            foreach( $items as $item ){
-                $i = $menu->items()->firstOrCreate($item);
-                $i->is_active = 1;
-                $i->save();
+            foreach ($items as $item) {
+                $row = $menu->items()->firstOrCreate(array_except($item, ['name', 'value', 'parameters']));
+
+                $translations = [];
+                foreach (TransHelper::getAllLanguages() as $language) {
+                    $translations[$language->iso_code] = [
+                        'name'       => $item['name'],
+                        'value'      => $item['value'],
+                        'parameters' => $item['parameters']
+                    ];
+                }
+                $row->updateTranslations($translations);
             }
         }
     }
